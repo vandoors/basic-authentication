c74fa9d8d5520cdfb175da93a3758a6bad2c5887
vandoors <143453791+vandoors@users.noreply.github.com>
2024-05-03 18:29:17 -0500

    Add to protected component

---
+++ src/Protected.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { getCurrentUser } from "aws-amplify/auth";
+import Container from "./Container";
+import { useNavigate } from "react-router-dom";
+
+const Protected = (props) => {
+   const navigate = useNavigate();
+   useEffect(() => {
+      getCurrentUser().catch(() => {
+         navigate("/profile");
+      });
+   }, []);
+   return (
+      <Container>
+         <h1>Protected Route</h1>
+      </Container>
+   );
+};
+
+export default Protected;
