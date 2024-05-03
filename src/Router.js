2f9cd623f98977070d85dd156b1dbb4f6388463a
vandoors <143453791+vandoors@users.noreply.github.com>
2024-05-03 18:33:45 -0500

    Set up routing
    Using BrowserRouter instead of HashRouter

---
+++ src/Router.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Nav from "./Nav";
+import Public from "./Public";
+import Protected from "./Protected";
+import Profile from "./Profile";
+
+const AppRouter = () => {
+   const [current, setCurrent] = useState("home");
+
+   useEffect(() => {
+      setRoute();
+      window.addEventListener("popstate", setRoute);
+      return () => window.removeEventListener("popstate", setRoute);
+   }, []);
+
+   const setRoute = () => {
+      const location = window.location.pathname.split("/");
+      const pathname = location[location.length - 1];
+      setCurrent(pathname ? pathname : "home");
+   };
+
+   return (
+      <Router>
+         <Nav current={current} />
+         <Routes>
+            <Route exact path="/" element={<Public />} />
+            <Route exact path="/protected" element={<Protected />} />
+            <Route exact path="/profile" element={<Profile />} />
+            <Route element={<Public />} />
+         </Routes>
+      </Router>
+   );
+};
+
+export default AppRouter;
